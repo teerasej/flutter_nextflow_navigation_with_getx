b51844a576d38ef311112834d840757b84f73bd3
Teerasej <teerasej@gmail.com>
2025-03-08 22:05:53 +0700

    remove due to unused

--- lib/controllers/nav/navigation_controller.dart
+++ lib/controllers/nav/navigation_controller.dart
@@ -7,7 +7,6 @@ import 'package:get/get.dart';
 
 class NavigationController extends GetxController {
   var currentIndex = 0.obs;
-  final pages = ['/catalog', '/settings'];
 
   void changePage(int index) {
     currentIndex.value = index;
